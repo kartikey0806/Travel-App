f2ddfcfa6f5d2f1bd71ecb5b90ae2ab7aa9fbdd3
kartikey0806 <63206364+kartikey0806@users.noreply.github.com>
2020-09-10 20:38:11 +0530

    Create Offers.js

---
+++ screens/Offers.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { Icon } from 'react-native-elements';
+
+export default class Offers extends React.Component{
+    render() {
+        return(
+            <View style={{justifyContent:'center',alignItems:'center', paddingTop: 100}}>
+                <Text>Offers Screen</Text>
+            </View>
+        )
+    }
+}
