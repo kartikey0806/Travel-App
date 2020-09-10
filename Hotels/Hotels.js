d9036d4c87365912b94a73d07ca3e1e2347341c9
kartikey0806 <63206364+kartikey0806@users.noreply.github.com>
2020-09-10 20:44:46 +0530

    Create Hotels.js

---
+++ Hotels/Hotels.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import Home from '../screens/Home';
+import Main from '../screens/Main';
+import HotelSearch from './HotelSearch';
+import HotelDetails from './HotelDetails'
+import HotelReview from './HotelReview'
+
+const HotelsScreen = createSwitchNavigator({
+  Home: Home,
+  HotelSearch: HotelSearch,
+  HotelDetails: HotelDetails,
+  HotelReview: HotelReview 
+} , {initialRouteName:'HotelSearch'})
+
+const AppContainer = createAppContainer(HotelsScreen);
+
+class Hotels extends React.Component{
+
+    render() {
+        return(
+            <AppContainer />
+        )
+    }
+}
+
+export default Hotels;
