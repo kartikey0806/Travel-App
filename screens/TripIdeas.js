68dcf906d367e169e7f2482cd6009cadda7657b4
kartikey0806 <63206364+kartikey0806@users.noreply.github.com>
2020-09-10 20:38:36 +0530

    Create TripIdeas.js

---
+++ screens/TripIdeas.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { Icon } from 'react-native-elements';
+
+export default class TripIdeas extends React.Component{
+    render() {
+        return(
+            <View style={{justifyContent:'center',alignItems:'center', paddingTop: 100}}>
+                <Text>Trip Ideas Screen</Text>
+            </View>
+        )
+    }
+}
